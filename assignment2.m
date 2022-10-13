517a515156a748ba3deb13447420d367d180cff4
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-13 17:29:36 +0300

    Update assignment2.m

--- assignment2.m
+++ assignment2.m
@@ -1,8 +1,10 @@
 clear all;
 close all;
+% The ; helps to define columns of a matrix
 A= [ 0.1:0.1:0.7; 14 12 10 8 6 4 2 ;1 1 1 1 0 0 0;3 6 9 12 15 18 21];
 A
-
+% we use parenthesis () when calling out specific columns and rows from a certain matrix
+% in the case below we are calling out the 1st,2nd,3rd rows and 1st,2nd,3rd,4th columns
  B=A(1:3,1:4)
  C=A(2:4,1:7)
 A(1,3)=0;
